da81b0a374ffd1d851b5a2f3a6044a3c8aa88c9d
Sanjaikumar-sk <pajanysanjaikumar@gmail.com>
2024-12-03 15:04:13 +0530

    Hello world in Spring

---
+++ firstProject/My_first_project_spring/src/test/java/com/helloWorldSpring/My_first_project_spring/MyFirstProjectSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helloWorldSpring.My_first_project_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstProjectSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
