50a7fcea51ba45a0c7522d9895bc7b3da52af638
Sanjaikumar-sk <pajanysanjaikumar@gmail.com>
2025-01-23 09:52:25 +0530

    Spring security from DB with issues solved

--- Ecommerce/src/main/java/com/practice/ecommerce/Model/User.java
+++ Ecommerce/src/main/java/com/practice/ecommerce/Model/User.java
@@ -2,7 +2,13 @@ package com.practice.ecommerce.Model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class User {
 
--- Ecommerce/src/main/java/com/practice/ecommerce/Service/SecurityService.java
+++ Ecommerce/src/main/java/com/practice/ecommerce/Service/SecurityService.java
@@ -4,6 +4,7 @@ import com.practice.ecommerce.Model.PrincipalUser;
 import com.practice.ecommerce.Model.User;
 import com.practice.ecommerce.Repository.GetProductList;
 import com.practice.ecommerce.Repository.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class SecurityService implements UserDetailsService {
 
+    @Autowired
     private UserRepo repo;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
