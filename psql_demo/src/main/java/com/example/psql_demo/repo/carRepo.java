595fbb300b7f7dc1fce2d26547b91e121bc3ae7e
Sanjaikumar-sk <pajanysanjaikumar@gmail.com>
2025-10-02 19:55:19 +0530

    deleting PSQL file

--- psql_demo/src/main/java/com/example/psql_demo/repo/carRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.psql_demo.repo;
-
-import com.example.psql_demo.model.car;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface carRepo extends JpaRepository<car, Integer> {
-}
